ea9e631ffaf78a60afde314c507d7de603f5e928
M <141670764+17m08@users.noreply.github.com>
2025-12-28 16:31:13 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from dessiner_arbre_cours import *
+
+racine=Noeud("Gaufrette", Noeud("Charlie", Noeud("Augustin", None, Noeud("Bubulle")), Noeud("Flipper")), Noeud("Médor"))
+arbre=Arbre(racine)
+dessiner(arbre)
+-- Executer ce que vous voulez comme l'affichage de la hauteur, la taille, etc...
